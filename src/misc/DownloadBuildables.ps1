b455f2ece1cadc6b0fb6dc6980178ba04bf7323f
Scott Bilas <scottbilas@gmail.com>
2023-08-15 23:20:40 +0200

    Print warnings for orphans on either side

--- src/misc/DownloadBuildables.ps1
+++ src/misc/DownloadBuildables.ps1
@@ -36,23 +36,70 @@ function match($name) {
     return 'Other'
 }
 
+function NameAndVariant($text) {
+    $text -match '^(.*?)(?:\s+\((.*?)\))?$' | Out-Null
+    $Matches[1], $Matches[2]
+}
+
+$images = @{}
+Get-ChildItem (Join-Path $PSScriptRoot .. images buildables) | ForEach-Object {
+    $name, $variant = NameAndVariant ([IO.Path]::GetFileNameWithoutExtension($_.Name).Replace('_', ' '))
+
+    if (!$images[$name]) {
+        $images[$name] = New-List
+    }
+    if ($variant) {
+        $images[$name].Add($variant)
+    }
+}
+
 $buildables = [ordered]@{}
 $groupings.Keys | Sort-Object { $groupings[$_].sort } | ForEach-Object { $buildables[$_] = [ordered]@{} }
 
+$reported = @{}
 $html.SelectNodes("(//table[@class='wikitable'])[1]//tr//td[1]") | ForEach-Object {
     # cleanup scraped name
     $_.InnerText.Replace('FICS???MAS', '')
 } | Sort-Object | ForEach-Object {
     # split out the variant name if any
-    $name, $variant = $_ -split ' \('
-    @{ name = $name; variant = $variant -replace '\)', '' }
+    $name, $variant = NameAndVariant $_
+
+    if ($images.ContainsKey($name)) {
+        $variants = $images[$name]
+        if ($variant) {
+            if (!$variants.Remove($variant)) {
+                Write-Warning "No image for buildable $name ($variant)"
+            }
+            if (!$variants.Count) {
+                [void]$images.Remove($name)
+            }
+        }
+        else {
+            if ($variants.Count) {
+                Write-Warning "Multiple images for buildable $name"
+            }
+            [void]$images.Remove($name)
+        }
+    }
+    elseif (!$reported[$name]) {
+        Write-Warning "No images for buildable $name"
+        $reported[$name] = 1
+    }
+
+    @{ name = $name; variant = $variant }
 } | Group-Object name | ForEach-Object {
-    #if ($_.Name -eq "Catwalk Corner") { Wait-Debugger }
-
     $group = $buildables[(match $_.Name)]
-    $variants = $_.Group.variant
+    $variants = $_.Group.variant ?? @()
 
-    $group[$_.Name] = $variants.Length -gt 1 ? $variants : @()
+    $group[$_.Name] = $variants.Length -gt 1 ? ($variants | ForEach-Object { $_ ?? "" }) : @()
+}
+
+$images.Keys | Sort-Object | ForEach-Object{
+    $text = $_
+    if ($images.$_) {
+        $text += " ($($images.$_ -join ', '))"
+    }
+    Write-Warning "No buildable for image $text"
 }
 
 $buildables | ConvertTo-Json | Out-File (Join-Path $PSScriptRoot .. inspector buildables.json) -Encoding utf8
